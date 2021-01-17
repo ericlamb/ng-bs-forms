e88162d26ced0b15ca36633ab3542618ef99e414
Eric Lamb <extremenerd@gmail.com>
2021-01-17 16:48:22 -0500

    Fixes missing form validators.

--- projects/ng-bs-forms/src/public-api.ts
+++ projects/ng-bs-forms/src/public-api.ts
@@ -7,3 +7,4 @@ export * from './lib/ng-bs-forms.component';
 export * from './lib/ng-bs-forms.module';
 export * from './lib/bs-field-error/bs-field-error.component';
 export * from './lib/bs-form-field/bs-form-field.component';
+export * from './lib/form-validators';
